c69c58d6527883656dbbf9f8b547daf880ef64e5
Nico Madysa <nico.madysa@cern.ch>
2020-10-28 11:57:00 +0100

    Downgrade PyJAPC dependency.

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
         "gym ~= 0.17.3",
         "numpy ~= 1.17",
         "pjlsa ~= 0.2.2",
-        "pyjapc ~=  2.2",
+        "pyjapc ~=  2.0",
         "pyqtgraph ~= 0.11.0",
         "qt-lsa-selector ~= 0.0.1.dev0",
     ],
