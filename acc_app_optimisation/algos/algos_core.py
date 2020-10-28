75bcdf2e9dd4574904cb3a44c8f3f5ebbe662b4e
Verena Kain <verena.kain@cern.ch>
2020-10-28 13:48:48 +0100

    added cobyla

--- acc_app_optimisation/algos/algos_core.py
+++ acc_app_optimisation/algos/algos_core.py
@@ -2,7 +2,7 @@ from acc_app_optimisation.algos.algo_base import AlgoSingleBase
 import gym
 import numpy as np
 import pybobyqa
-
+from scipy.optimize import fmin_cobyla
 
 
 class BobyQaAlgo(AlgoSingleBase):
@@ -11,7 +11,21 @@ class BobyQaAlgo(AlgoSingleBase):
         self.env = env
         x_0 = env.get_initial_params()
         bounds_0 = (np.ones(env.optimization_space.shape[-1])*(-1.),np.ones(env.optimization_space.shape[-1])*1.)
-        self.opt_params ={"x0":x_0,"bounds":bounds_0,"rhobeg":1.,"seek_global_minimum":False,"maxfun":100, "rhoend":0.1,"objfun_has_noise":False}
-        self.opt_callback = pybobyqa.solve
+        self.opt_params ={"x0":x_0,"bounds":bounds_0,"rhobeg":1.,"seek_global_minimum":False,"maxfun":100, "rhoend":0.05,"objfun_has_noise":False}
+        self.opt_callback = lambda *args, **kwargs: pybobyqa.solve(*args, **kwargs).x
 
-all_single_algos_dict = {"BOBYQA":BobyQaAlgo}
+
+class CobylaAlgo(AlgoSingleBase):
+    def constr1(self,x):
+        return 1. -np.abs(x)
+
+    def __init__(self,env:gym.Env):
+        super(AlgoSingleBase, self).__init__()
+        self.env = env
+        x_0 = env.get_initial_params()
+        bounds_0 = (np.ones(env.optimization_space.shape[-1])*(-1.),np.ones(env.optimization_space.shape[-1])*1.)
+        self.opt_params ={"x0":x_0,"cons":[self.constr1],"rhobeg":1.,"maxfun":100, "rhoend":0.05}
+        self.opt_callback = fmin_cobyla
+
+
+all_single_algos_dict = {"BOBYQA":BobyQaAlgo,"COBYLA":CobylaAlgo}
