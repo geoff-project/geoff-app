71b4fc352704270f04922f034fac99f5f736dda4
Nico Madysa <nico.madysa@cern.ch>
2020-11-04 17:00:55 +0100

    Refactor config widgets.

--- acc_app_optimisation/gui/config_widget.py
+++ acc_app_optimisation/gui/config_widget.py
@@ -78,61 +78,111 @@ class ConfigureDialog(QDialog):
         """Given a field, pick the best widget to configure it."""
         # pylint: disable = too-many-return-statements
         if field.choices is not None:
-            widget = QComboBox()
-            widget.addItems(str(choice) for choice in field.choices)
-            widget.setCurrentText(str(field.value))
-            widget.currentTextChanged.connect(self._make_setter(field))
-            return widget
+            return _combobox(field, self.current_values)
         if field.range is not None:
-            # low, high = field.range
-            # if isinstance(field.value, (int, np.integer)):
-            #     widget = QSpinBox()
-            # elif isinstance(field.value, (float, np.floating)):
-            #     widget = QDoubleSpinBox()
-            # else:
-            #     raise KeyError(type(field.value))
-            # widget.setValue(field.value)
-            # widget.setRange(low, high)
-            # widget.valueChanged.connect(self._make_setter(field))
-            widget = QLineEdit(str(field.value))
-            widget.setValidator(QDoubleValidator())
-            widget.editingFinished.connect(self._make_setter(field, get=widget.text))
-            return widget
-        if isinstance(field.value, (bool, np.bool_)):
-            widget = QCheckBox()
-            widget.setChecked(field.value)
-            widget.stateChanged.connect(self._make_setter(field))
-            return widget
-        if isinstance(field.value, (int, np.integer)):
-            widget = QLineEdit(str(field.value))
-            widget.setValidator(QIntValidator())
-            widget.editingFinished.connect(self._make_setter(field, get=widget.text))
-            return widget
-        if isinstance(field.value, (float, np.floating)):
-            widget = QLineEdit(str(field.value))
-            widget.setValidator(QDoubleValidator())
-            widget.editingFinished.connect(self._make_setter(field, get=widget.text))
-            return widget
-        if isinstance(field.value, str):
-            widget = QLineEdit(str(field.value))
-            widget.editingFinished.connect(self._make_setter(field, get=widget.text))
+            low, high = field.range
+            if _is_float(field.value) and abs(high) / abs(low) > 1000.0:
+                widget = _maybe_lineedit(field, self.current_values)
+                assert widget is not None
+                return widget
+            return _spinbox(field, self.current_values)
+        if _is_bool(field.value):
+            return _checkbox(field, self.current_values)
+        widget = _maybe_lineedit(field, self.current_values)
+        if widget is not None:
             return widget
         return QLabel(str(field.value))
 
-    def _make_setter(
-        self,
-        field: coi.Config.Field,
-        get: t.Optional[t.Callable[[], str]] = None,
-    ):
-        """Return a callback that can be used to update a field's value."""
 
-        def _setter(value=None):
-            if get is not None:
-                value = get()
-            if isinstance(field.value, (bool, np.bool_)):
-                value = "checked" if value else ""
-            else:
-                value = str(value)
-            self.current_values[field.dest] = value
+def _checkbox(field: coi.Config.Field, values: t.Dict[str, str]) -> QWidget:
+    """Create a check box."""
+    widget = QCheckBox()
+    widget.setChecked(field.value)
+    widget.stateChanged.connect(_make_setter(field, values))
+    return widget
 
-        return _setter
+
+def _combobox(field: coi.Config.Field, values: t.Dict[str, str]) -> QWidget:
+    """Create a combo box."""
+    widget = QComboBox()
+    widget.addItems(str(choice) for choice in field.choices)
+    widget.setCurrentText(str(field.value))
+    widget.currentTextChanged.connect(_make_setter(field, values))
+    return widget
+
+
+def _maybe_lineedit(
+    field: coi.Config.Field, values: t.Dict[str, str]
+) -> t.Optional[QWidget]:
+    """Create a line edit that may contain strings, integers or floats."""
+    widget = QLineEdit(str(field.value))
+    if _is_int(field.value):
+        widget.setGroupSeparatorShown(True)
+        widget.setValidator(QIntValidator())
+    elif _is_float(field.value):
+        widget.setGroupSeparatorShown(True)
+        widget.setValidator(QDoubleValidator())
+    elif isinstance(field.value, str):
+        pass
+    else:
+        return None
+    widget.editingFinished.connect(_make_setter(field, values, get=widget.text))
+    return widget
+
+
+def _spinbox(field: coi.Config.Field, values: t.Dict[str, str]) -> QWidget:
+    """Create either an integer or a floating-point spin box."""
+    if _is_int(field.value):
+        widget = QSpinBox()
+    elif _is_float(field.value):
+        widget = QDoubleSpinBox()
+    else:
+        raise KeyError(type(field.value))
+    widget.setValue(field.value)
+    widget.setStepType(widget.AdaptiveDecimalStepType)
+    widget.setGroupSeparatorShown(True)
+    low, high = field.range
+    decimals = 1 + max(2, np.ceil(-np.log10(low)), np.ceil(-np.log10(high)))
+    widget.setDecimals(decimals)
+    widget.setRange(low, high)
+    widget.valueChanged.connect(_make_setter(field, values))
+    return widget
+
+
+def _make_setter(
+    field: coi.Config.Field,
+    values: t.Dict[str, str],
+    get: t.Optional[t.Callable[[], str]] = None,
+):
+    """Return a callback that can be used to update a field's value."""
+    if isinstance(field.value, (bool, np.bool_)):
+        map_value = lambda v: "checked" if v else ""
+    else:
+        map_value = str
+
+    if get is not None:
+
+        def _setter():
+            values[field.dest] = map_value(get())
+
+    else:
+
+        def _setter(value):
+            values[field.dest] = map_value(value)
+
+    return _setter
+
+
+def _is_int(value: t.Any) -> bool:
+    """Return True if `value` is a Python or NumPy int."""
+    return isinstance(value, (int, np.integer))
+
+
+def _is_float(value: t.Any) -> bool:
+    """Return True if `value` is a Python or NumPy float."""
+    return isinstance(value, (float, np.floating))
+
+
+def _is_bool(value: t.Any) -> bool:
+    """Return True if `value` is a Python or NumPy bool."""
+    return isinstance(value, (bool, np.bool_))
