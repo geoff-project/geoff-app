fecad4dbf2ed577a254c9520c0100298c13c39aa
Nico Madysa <nico.madysa@cern.ch>
2020-11-25 17:38:35 +0100

    Document utils.accelerators.

--- acc_app_optimisation/utils/accelerators.py
+++ acc_app_optimisation/utils/accelerators.py
@@ -1,16 +1,21 @@
+#!/usr/bin/env python
+"""Utilities for dealing with various accelerator naming conventions."""
+
 from enum import Enum
 
 from cernml.coi import Machine
 
 
 class IncaAccelerators(Enum):
+    """Mapping between INCA, COI and LSA-widget accelerator names."""
+
     def __init__(self, lsa_name: str, machine: Machine) -> None:
         self.lsa_name = lsa_name
         self.machine = machine
 
     SPS = "sps", Machine.SPS
     PSB = "psb", Machine.PSB
-    PS = "ps", Machine.PS
+    PS = "ps", Machine.PS  # pylint: disable=invalid-name
     LEIR = "leir", Machine.Leir
     Awake = "awake", Machine.Awake
     # Dirty hack: The LSA selector widget can't handle Linac3, so we replace it
