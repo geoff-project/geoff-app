7289bed34cf597b4de8f6465560d07e2e04c308d
Nico Madysa <nico.madysa@cern.ch>
2023-04-27 11:26:05 +0200

    Add job_control.single_objective.__all__.

--- acc_app_optimisation/job_control/single_objective/__init__.py
+++ acc_app_optimisation/job_control/single_objective/__init__.py
@@ -4,3 +4,12 @@ from . import optimizers
 from .builder import CannotBuildJob, OptJobBuilder
 from .jobs import OptJob
 from .optimizers import ALL_OPTIMIZERS, OptimizerFactory
+
+__all__ = [
+    "ALL_OPTIMIZERS",
+    "CannotBuildJob",
+    "OptJob",
+    "OptJobBuilder",
+    "OptimizerFactory",
+    "optimizers",
+]
