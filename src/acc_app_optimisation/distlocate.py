bfcc62db484ef76831f3240128c25c3a00c1910b
Nico Madysa <nico.madysa@cern.ch>
2023-07-20 18:08:30 +0200

    Handle editable installs in `find_distribution()`.
    Editable installs do not contain the target file at all, since the file
    typically resides in a project directory outside of the PYTHONPATH.
    
    However, editable installs point to that directory via *path
    configuration files* (`*.pth`). We can read these files, follow the
    paths that they point to, and then resolve the target file relative to
    them.

--- src/acc_app_optimisation/distlocate.py
+++ src/acc_app_optimisation/distlocate.py
@@ -6,6 +6,7 @@
 
 """Locate distributions that provide a certain type."""
 import inspect
+import os
 import sys
 import typing as t
 from dataclasses import dataclass
@@ -45,19 +46,65 @@ def find_distribution(class_: type) -> t.Optional[DistInfo]:
 def dist_provides(dist: metadata.Distribution, file_path: Path) -> bool:
     """Return True if `dist` provides the given file.
 
-    This works even if `dist` is installed as a zip file.
+    This works even if `dist` is installed as a zip file or an editable
+    install.
     """
     install_dir = get_install_dir(dist)
     try:
         relative_path = file_path.relative_to(install_dir)
     except ValueError:
-        # The paths are not relative to each other.
-        return False
+        # The paths are not relative to each other. One reason could be
+        # that `dist` is an editable install and `file_path` points at
+        # the original location. In this case, we have to search `dist`
+        # for *.pth files and see if one points at an ancestor of
+        # `file_path`.
+        return any(map(file_path.is_relative_to, iter_path_config_paths(dist)))
     if dist.files is None:
         # This may happen if the distribution, for whatever reason,
         # doesn't specify its files.
         return False
     return relative_path in dist.files
+
+
+def iter_path_config_paths(dist: metadata.Distribution) -> t.Iterator[os.PathLike]:
+    """Return an iterator over paths indirectly contained by this distribution.
+
+    If *dist* contains :doc:`path configuration files
+    <std:/library/site>`, this iterates over all paths pointed to  by
+    these files. This allows resolving editable installs as supported by
+    Pip.
+
+    This returns only paths that exist, though they need not be
+    directories. This also ignores import lines in all path
+    configuration files.
+    """
+    if dist.files is None:
+        # This may happen if the distribution, for whatever reason,
+        # doesn't specify its files.
+        return
+    for path in dist.files:
+        if path.suffix == ".pth":
+            yield from filter(
+                os.path.exists,
+                map(dist.locate_file, read_path_config_file(path.locate())),
+            )
+
+
+def read_path_config_file(path: os.PathLike) -> t.List[str]:
+    """Read a :path:`*.pth` file and return the paths it points to.
+
+    This correctly removes empty and comment lines. It incorrectly
+    ignores import lines and does not check paths for existence.
+    Relative paths stay relative when returned.
+    """
+    # pylint: disable = unspecified-encoding
+    with open(os.fspath(path), "r") as file:
+        non_empty_lines = filter(None, map(str.strip, file))
+        return [
+            line
+            for line in non_empty_lines
+            if line.split()[0] != "import" and not line.startswith("#")
+        ]
 
 
 def get_install_dir(dist: metadata.Distribution) -> Path:
@@ -77,8 +124,8 @@ def get_install_dir(dist: metadata.Distribution) -> Path:
 def get_file_path(class_: type) -> t.Optional[Path]:
     """Get the path of the file that defines a class.
 
-    This returns `None` if no file can be aassociated with the type.
-    This happens for built-in types and those defined on the interactive
+    This returns `None` if no file can be associated with the type. This
+    happens for built-in types and those defined on the interactive
     interpreter.
     """
     # If the module is compressed, this is a path _into_ the archive
